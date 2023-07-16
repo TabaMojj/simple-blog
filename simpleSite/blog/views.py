3c8cfac513ab8eefede90b2d0717321592daff06
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-07-16 15:16:20 +0330

    Add post tagging functionality

--- simpleSite/blog/views.py
+++ simpleSite/blog/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
-from .models import Post, Comment
+from .models import Post
+from taggit.models import Tag
 from .forms import EmailPostForm, CommentForm
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
     paginator = Paginator(object_list, 2)
     page_number = request.GET.get('page', 1)
     try:
@@ -16,7 +21,7 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     except PageNotAnInteger:
         posts = paginator.page(1)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html', {'posts': posts, 'tag': tag})
 
 
 def post_detail(request, post, year, month, day):
