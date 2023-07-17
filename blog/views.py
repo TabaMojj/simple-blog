8646f8097539a8d70d5c060ed052e37911f1f56e
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-07-17 13:37:45 +0330

    Add search functionality

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,9 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from .models import Post
 from taggit.models import Tag
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 def post_list(request, tag_slug=None):
@@ -47,8 +48,6 @@ def post_share(request, post_id):
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            print("**1**: ", post.get_absolute_url())
-            print("**2**: ", request.build_absolute_uri(post.get_absolute_url()))
             post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = f"{cd['name']} recommends you read " \
                       f"{post.title}"
@@ -72,3 +71,20 @@ def post_comment(request, post_id):
         comment.post = post
         comment.save()
     return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(search=search_vector, rank=SearchRank(search_vector, search_query))\
+                .filter(search=search_query).order_by('-rank')
+
+    return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
